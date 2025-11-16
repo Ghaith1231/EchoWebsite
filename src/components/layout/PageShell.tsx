112856f8a31cb07a632f46f44f038124369288fa
Ghaih1231 <2315539@leedstrinity.ac.uk>
2025-11-16 20:56:05 +0000

    Fix layout shell and imports

---
+++ src/components/layout/PageShell.tsx
@@ -0,0 +1,22 @@
+import React, { ReactNode } from "react";
+
+
+
+type PageShellProps = {
+  children: ReactNode;
+};
+
+export function PageShell({ children }: PageShellProps) {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        padding: "2rem",
+      }}
+    >
+      {children}
+    </div>
+  );
+}
